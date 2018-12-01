342f7292558c7516c1eec3d000b1f02a1f4dd58e
zhanghaiquan <zhqustb@163.com>
2018-12-02 00:23:01 +0800

    pull

--- controller/melt.js
+++ controller/melt.js
@@ -10,7 +10,7 @@ class Melt {
     this.updateData = this.updateData.bind(this);
   }
   async queryData(req, res, next) {
-    const castId = req.query.castId;
+    const { castId, current = 1, limit = 20 } = req.query;
     try{
       if (!castId) {
         throw new Error('参数错误')
@@ -21,16 +21,22 @@ class Melt {
         status: 1,
         message: err.message
       })
-      return
+      return;
     }
     try {
-      const list = await meltModel.find({castId});
+      // let count = await meltModel.count({castId});
+      const count = await meltModel.estimatedDocumentCount({castId});
+      const totalPage = Math.ceil(count / limit);
+      const list = await meltModel.find({castId}).skip((current - 1) * limit).limit(limit).sort({'furnace': 'desc'});
       // 要考虑分页
       res.send({
         status: 0,
         message: '操作成功',
         data: {
-          list: list
+          count,
+          current,
+          totalPage, 
+          list
         }
       });
     } catch (error) {
