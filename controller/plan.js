9a72b230dac73a9c2d52b9bd1e106ebbe5e02408
张海泉 <haiquan.zhang@tcredit.com>
2018-12-09 23:43:39 +0800

    update

--- controller/plan.js
+++ controller/plan.js
@@ -25,33 +25,31 @@ class Plan {
       return;
     }
     try {
-      const count = await planModel.estimatedDocumentCount({castId});
-      const totalPage = Math.ceil(count / limit);
-      const list = await planModel.find({castId}).skip((current - 1) * limit).limit(limit).sort({'furnace': 'desc'});
-      // 要考虑分页
+      const list = await planModel.find({date, castId}).sort({'furnace': 'desc'});
+      // 当前炉次的大盘毛重，从各个机组的喷带记录表中查询
+
+      // rawWeight
+
       res.send({
         status: 0,
         message: '操作成功',
         data: {
-          count,
-          current,
-          totalPage,
-          limit,
           list
         }
       });
     } catch (err) {
-      console.log('查询化钢记录失败', err);
+      console.log('查询生产计划失败', err);
       res.send({
         status: -1,
-        message: '查询化钢记录失败'
+        message: '查询生产计划失败'
       });
     }
   }
+
   async createData(req, res, next) {
-    const { castId, furnace, ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace, newAlloyNumber = '', newAlloyWeight = 0, oldAlloyNumber = '', oldAlloyWeight = 0, mixAlloyNumber = '', mixAlloyWeight = 0, hignNbNumber = '', hignNbWeight = 0, Si = 0, Ni = 0, Cu = 0, BFe = 0, NbFe = 0, alloyTotalWeight = 0, alloyOutWeight = 0, alloyFixWeight = 0, remark = '' } = req.body;
+    const { date, castId, remark = '', fileNumber = '', team, taskOrder = '', ribbonTypeId, ribbonTypeName, client = '', thickness, laminationFactor, furnace, alloyWeight = 0, castTime = '' } = req.body;
     try{
-      if (!castId || !furnace || !ribbonTypeId || !ribbonTypeName || !bucket || !melter || !meltFurnace) {
+      if (!date || !castId || !team || !ribbonTypeId || !ribbonTypeName || !thickness || !furnace || !laminationFactor) {
         throw new Error('参数错误')
       }
     }catch(err){
@@ -64,7 +62,7 @@ class Plan {
     }
 
     try {
-      const data = await castModel.findOne({ furnace });
+      const data = await planModel.findOne({ furnace });
       // 如果没有查到则返回值为 null， 如果查询到则返回值为一个对象
       if (data) {
         throw new Error('炉号重复');
@@ -80,33 +78,29 @@ class Plan {
 
     try {
       const newData = {
-        castId, furnace,
-        ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace,
-        newAlloyNumber, newAlloyWeight,
-        oldAlloyNumber, oldAlloyWeight,
-        mixAlloyNumber, mixAlloyWeight,
-        hignNbNumber, hignNbWeight,
-        Si, Ni, Cu, BFe, NbFe, 
-        alloyTotalWeight, alloyOutWeight, alloyFixWeight,
-        remark
+        date, remark, fileNumber,
+        castId, team, taskOrder,
+        ribbonTypeId, ribbonTypeName, client,
+        thickness, laminationFactor, furnace,
+        alloyWeight, castTime
       };
       await planModel.create(newData);
       res.send({
         status: 0,
-        message: '新增化钢记录成功'
+        message: '新增生产记录成功'
       });
     } catch (err) {
-      console.log('新增化钢记录失败', err);
+      console.log('新增生产记录失败', err);
       res.send({
         status: -1,
-        message: `新增化钢记录失败, ${err.message}`
+        message: `新增生产记录失败, ${err.message}`
       });
     }
   }
   async updateData(req, res, next) {
-    const { castId, furnace, ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace, newAlloyNumber = '', newAlloyWeight = 0, oldAlloyNumber = '', oldAlloyWeight = 0, mixAlloyNumber = '', mixAlloyWeight = 0, hignNbNumber = '', hignNbWeight = 0, Si = 0, Ni = 0, Cu = 0, BFe = 0, NbFe = 0, alloyTotalWeight = 0, alloyOutWeight = 0, alloyFixWeight = 0, remark = '' } = req.body;
+    const { date, castId, remark = '', fileNumber = '', team, taskOrder = '', ribbonTypeId, ribbonTypeName, client = '', thickness, laminationFactor, furnace, alloyWeight = 0, castTime = '' } = req.body;
     try{
-      if (!castId || !furnace || !ribbonTypeId || !ribbonTypeName || !bucket || !melter || !meltFurnace) {
+      if (!date || !castId || !team || !ribbonTypeId || !ribbonTypeName || !thickness || !furnace || !laminationFactor) {
         throw new Error('参数错误')
       }
     }catch(err){
@@ -119,26 +113,22 @@ class Plan {
     }
     try {
       const newData = {
-        castId, furnace,
-        ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace,
-        newAlloyNumber, newAlloyWeight,
-        oldAlloyNumber, oldAlloyWeight,
-        mixAlloyNumber, mixAlloyWeight,
-        hignNbNumber, hignNbWeight,
-        Si, Ni, Cu, BFe, NbFe,
-        alloyTotalWeight, alloyOutWeight, alloyFixWeight,
-        remark
+        date, remark, fileNumber,
+        castId, team, taskOrder,
+        ribbonTypeId, ribbonTypeName, client,
+        thickness, laminationFactor, furnace,
+        alloyWeight, castTime
       };
       await planModel.updateOne({ furnace }, { $set: newData });
       res.send({
         status: 0,
-        message: '更新化钢记录成功'
+        message: '更新生产计划成功'
       });
     } catch (err) {
-      console.log('更新化钢记录失败', err);
+      console.log('更新生产计划失败', err);
       res.send({
         status: -1,
-        message: `更新化钢记录失败, ${err.message}`
+        message: `更新生产计划失败, ${err.message}`
       });
     }
   }
--- models/plan.js
+++ models/plan.js
@@ -7,29 +7,25 @@ const planSchema = new Schema({
   date: String,//排产日期 2018-12-01
   remark: String,//备注
   fileNumber: String,//文件编号 QEHAT-FJ/D-C03-036-C/0
-  taskList: [
-    {
-      castId: Number, // 机组编号
-      team: String,// 班组 早
-      taskOrder: String, //任务单号
-      ribbonTypeId: Number, // 材质id
-      ribbonTypeName: String,//材质名称 1k107B
-      client: String, //客户 
-      thickness: String, //带材厚度要求  23-24
-      laminationFactor: String, // 叠片系数的要求 > 0.80
-      furnace: String, //制带炉号 06-20181120-01/01
-      alloyWeight: Number, // 单炉投入重量 KG
-      castTime: String, //计划喷带时间 8:00-10:00
-      rawWeight: Number // 实际产出的大盘毛重 KG
-    }
-  ]
+  castId: Number, // 机组编号
+  team: String,// 班组 早
+  taskOrder: String, //任务单号
+  ribbonTypeId: Number, // 材质id
+  ribbonTypeName: String,//材质名称 1k107B
+  client: String, //客户 
+  thickness: String, //带材厚度要求  23-24
+  laminationFactor: String, // 叠片系数的要求 > 0.80
+  furnace: String, //制带炉号 06-20181120-01/01
+  alloyWeight: Number, // 单炉投入重量 KG
+  castTime: String, //计划喷带时间 8:00-10:00
+  rawWeight: Number // 实际产出的大盘毛重 KG
 }, {
 	collection: 'Plan',
 	timestamps: true,
 	autoIndex: false
 });
 
-planSchema.index({furnace: 1});
+planSchema.index({date: 1});
 
 const Plan = mongoose.model('Plan', planSchema);
 
