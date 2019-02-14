2c458db111db5a84313b9c20b26155647b1adc97
张海泉 <haiquan.zhang@tcredit.com>
2019-02-14 23:02:20 +0800

    update

--- controller/measure.js
+++ controller/measure.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const measureModel = require('../models/measure');
+const castModel = require('../models/cast');
 
 class Measure {
   constructor() {
@@ -8,7 +9,7 @@ class Measure {
   }
 
   async queryData(req, res, next) {
-    const { castId, startTime, endTime, melter, current = 1, limit = 10 } = req.query;
+    const { castId, startTime, endTime, caster, current = 1, limit = 10 } = req.query;
     try{
       if (!castId) {
         throw new Error('参数错误')
@@ -23,15 +24,15 @@ class Measure {
     }
     try {
       let queryCondition = {castId};
-      if (melter) {
-        queryCondition.melter = melter;
+      if (caster) {
+        queryCondition.caster = caster;
       }
       if (startTime && endTime) {
         queryCondition.createdAt = { $gt: startTime, $lt: endTime };
       }
       const count = await measureModel.countDocuments(queryCondition);
       const totalPage = Math.ceil(count / limit);
-      const list = await measureModel.find(queryCondition).skip((current - 1) * limit).limit(limit).sort({'furnace': 'desc'});
+      const list = await measureModel.find(queryCondition).skip((current - 1) * limit).limit(limit).sort({'furnace': 'desc', 'coilNumber': 'desc'});
       // 要考虑分页
       res.send({
         status: 0,
@@ -45,17 +46,17 @@ class Measure {
         }
       });
     } catch (err) {
-      console.log('查询化钢记录失败', err);
+      console.log('查询检测记录失败', err);
       res.send({
         status: -1,
-        message: '查询化钢记录失败'
+        message: '查询检测记录失败'
       });
     }
   }
   async createData(req, res, next) {
-    const { castId, furnace, ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace, newAlloyNumber = '', newAlloyWeight = 0, oldAlloyNumber = '', oldAlloyWeight = 0, mixAlloyNumber = '', mixAlloyWeight = 0, hignNbNumber = '', hignNbWeight = 0, Si = 0, Ni = 0, Cu = 0, BFe = 0, NbFe = 0, alloyTotalWeight = 0, alloyOutWeight = 0, alloyFixWeight = 0, remark = '' } = req.body;
+    const { castId, furnace, coilNumber, diameter, coilWeight, laminationFactor, laminationLevel } = req.body;
     try{
-      if (!castId && !furnace && !ribbonTypeId && !ribbonTypeName && !bucket && !melter && !meltFurnace) {
+      if (!castId && !furnace && !coilNumber && !diameter && !coilWeight) {
         throw new Error('参数错误')
       }
     }catch(err){
@@ -68,10 +69,10 @@ class Measure {
     }
 
     try {
-      const data = await measureModel.findOne({ furnace });
+      const data = await measureModel.findOne({ furnace, coilNumber });
       // 如果没有查到则返回值为 null， 如果查询到则返回值为一个对象
       if (data) {
-        throw new Error('炉号重复');
+        throw new Error('炉号和盘号重复');
       }
     } catch (err) {
       console.log(err.message, err);
@@ -83,27 +84,24 @@ class Measure {
     }
 
     try {
+      // 根据炉号获取其他信息。但是如果furnace相同的话，可以只执行第一次查询，后面都使用缓存结果，此处需优化
+      const { ribbonTypeName, ribbonWidth, createdAt, caster } = castModel.findOne({ furnace });
       const newData = {
-        castId, furnace,
-        ribbonTypeId, ribbonTypeName, bucket, melter, meltFurnace,
-        newAlloyNumber, newAlloyWeight,
-        oldAlloyNumber, oldAlloyWeight,
-        mixAlloyNumber, mixAlloyWeight,
-        hignNbNumber, hignNbWeight,
-        Si, Ni, Cu, BFe, NbFe, 
-        alloyTotalWeight, alloyOutWeight, alloyFixWeight,
-        remark
+        castId, furnace, ribbonTypeName, ribbonWidth, caster,
+        castDate: createdAt,
+        coilNumber, diameter, coilWeight,
+        laminationFactor, laminationLevel
       };
       await measureModel.create(newData);
       res.send({
         status: 0,
-        message: '新增化钢记录成功'
+        message: '新增重卷记录成功'
       });
     } catch (err) {
-      console.log('新增化钢记录失败', err);
+      console.log('新增重卷记录失败', err);
       res.send({
         status: -1,
-        message: `新增化钢记录失败, ${err.message}`
+        message: `新增重卷记录失败, ${err.message}`
       });
     }
   }
@@ -167,15 +165,15 @@ class Measure {
       if (n != 0) {
         res.send({
           status: 0,
-          message: '删除化钢记录成功'
+          message: '删除检测记录成功'
         });
       } else {
-        throw new Error('删除化钢记录失败');
+        throw new Error('删除检测记录失败');
       }
     } catch (error) {
       res.send({
         status: -1,
-        message: '删除化钢记录失败'
+        message: '删除检测记录失败'
       });
     }
   }
--- models/measure.js
+++ models/measure.js
@@ -4,32 +4,43 @@ const mongoose =  require('mongoose');
 const Schema = mongoose.Schema;
 
 const measureSchema = new Schema({
-	castId: Number,// 机组编号
-	furnace: String,// 制带炉号 06-20181120-01/01
-	ribbonTypeId: Schema.Types.ObjectId, // 材质id
-	ribbonTypeName: String,//材质名称
-	bucket: String,// 配料桶号
-	melter: String,// 熔炼人
-	meltFurnace: String,// 冶炼炉炉号 A 或 B
-	newAlloyNumber: String,// 新料炉号 新B-2018-12-12
-	newAlloyWeight: Number,// 新料重量
-	oldAlloyNumber: String,// 加工料炉号 加B-2018-12-12
-	oldAlloyWeight: Number,//加工料重量
-	mixAlloyNumber: String,// 回炉锭炉号
-	mixAlloyWeight: Number,// 回炉锭重量
-	highNbNumber: String,// 高铌料炉号
-	highNbWeight: Number,// 高铌料重量
-	Si: Number,// 硅重量
-	Ni: Number,// Ni重量
-	Cu: Number,// Cu重量
-	BFe: Number,// 硼铁硅重量
-	NbFe: Number,// 铌铁重量
-	alloyTotalWeight: Number,//总重量
-	alloyOutWeight: Number,// 放钢重量
-	alloyFixWeight: Number,// 总重量修正
-	remark: String // 备注
+  castId: Number,// 机组编号
+  // 重卷录入信息
+  furnace: String,// 制带炉号 06-20181120-01/01
+  ribbonTypeName: String,//材质名称
+  ribbonWidth: Number,//带宽
+  castDate: Date, //生产日期
+  caster: String, //喷带手
+  coilNumber: Number, // 盘号 1, 2, 3
+  diameter: Number, // 外径
+  coilWeight: Number, // 单盘重量
+  laminationFactor: Number, //叠片系数 0.80
+  laminationLevel: Number, //叠片等级 1, 2, 3, 4
+  // 检测录入信息
+  realRibbonWidth: Number,//实际带宽
+  ribbenThickness1: Number, //带材厚度点1, μm
+  ribbenThickness2: Number, //带材厚度点2, μm
+  ribbenThickness3: Number, //带材厚度点3, μm
+  ribbenThickness4: Number, //带材厚度点4, μm
+  ribbenThickness5: Number, //带材厚度点5, μm
+  ribbenThickness6: Number, //带材厚度点6, μm
+  ribbenThickness7: Number, //带材厚度点7, μm
+  ribbenThickness8: Number, //带材厚度点8, μm
+  ribbenThickness9: Number, //带材厚度点9, μm
+  ribbenThicknessDeviation: Number, // 带材横向偏差, μm
+  ribbenThickness: Number, // 带材平均厚度, μm
+  ribbenThicknessLevel: Number, // 带材厚度级别
+  ribbenToughness: String, //带材韧性
+  ribbenToughnessLevel: String, //带材韧性等级
+  appearence: String, //带材外观
+  appearenceLevel: String, //带材外观等级
+  ribbenTotalLevel: String, //带材综合级别
+  storageRule: String, //入库规则
+  isStored: Number, // 是否入库，1-是，0-否
+  operation: String, //操作
+  clients: String //去向 德国，法国
 }, {
-	collection: 'Melt',
+	collection: 'Measure',
 	timestamps: true,
 	autoIndex: false
 });
