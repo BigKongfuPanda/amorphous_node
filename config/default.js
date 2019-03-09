0ee8a63ccf8d29429212cea2eeac1f974b888f24
张海泉 <haiquan.zhang@tcredit.com>
2019-03-09 12:37:01 +0800

    update

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	hostname: '127.0.0.1',
+	hostname: '192.168.0.122',
 	port: parseInt(process.env.PORT, 10) || 8001,
 	url: 'mongodb://localhost:27017/amorphous',
 	session: {
