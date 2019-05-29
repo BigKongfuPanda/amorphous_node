2d1e70492248265c8d3b25bf5066d9bb47c1e30d
zhanghaiquan <zhqustb@163.com>
2019-05-30 00:43:09 +0800

    update

--- models/melt.js
+++ models/melt.js
@@ -1,44 +1,46 @@
 'use strict';
 
-const mongoose =  require('mongoose');
-const Schema = mongoose.Schema;
+const Sequelize =  require('sequelize');
+const sequelize = require('../mysql/db');
 
-const meltSchema = new Schema({
-	castId: Number,// 机组编号
-	furnace: String,// 制带炉号 06-20181120-01/01
-	ribbonTypeId: Schema.Types.ObjectId, // 材质id
-	ribbonTypeName: String,//材质名称
-	bucket: String,// 配料桶号
-	melter: String,// 熔炼人
-	meltFurnace: String,// 冶炼炉炉号 A 或 B
-	newAlloyNumber: String,// 新料炉号 新B-2018-12-12
-	newAlloyWeight: Number,// 新料重量
-	oldAlloyNumber: String,// 加工料炉号 加B-2018-12-12
-	oldAlloyWeight: Number,//加工料重量
-	mixAlloyNumber: String,// 回炉锭炉号
-	mixAlloyWeight: Number,// 回炉锭重量
-	highNbNumber: String,// 高铌料炉号
-	highNbWeight: Number,// 高铌料重量
-	Si: Number,// 硅重量
-	Ni: Number,// Ni重量
-	Cu: Number,// Cu重量
-	BFe: Number,// 硼铁硅重量
-	NbFe: Number,// 铌铁重量
-	alloyTotalWeight: Number,//总重量
-	alloyOutWeight: Number,// 放钢重量
-	alloyFixWeight: Number,// 总重量修正
-	updatePerson: String, // 更新者
-	createPerson: String, // 创建者
-	remark: String, // 备注
-	createTime: Date // 冶炼时间，00:00:00 - 08:00:00 之间的算上一天，不能算当天
-}, {
-	collection: 'Melt',
-	timestamps: true,
-	autoIndex: false
+const Melt = sequelize.define('melt', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true
+	},
+	castId: Sequelize.INTEGER,// 机组编号
+	furnace: {
+		type: Sequelize.Sequelize.STRING,
+		primaryKey: true
+	},// 制带炉号 06-20181120-01/01
+	ribbonTypeId: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true
+	}, // 材质id
+	ribbonTypeName: Sequelize.STRING,//材质名称
+	bucket: Sequelize.STRING,// 配料桶号
+	melter: Sequelize.STRING,// 熔炼人
+	meltFurnace: Sequelize.STRING,// 冶炼炉炉号 A 或 B
+	newAlloyNumber: Sequelize.STRING,// 新料炉号 新B-2018-12-12
+	newAlloyWeight: Sequelize.FLOAT,// 新料重量
+	oldAlloyNumber: Sequelize.STRING,// 加工料炉号 加B-2018-12-12
+	oldAlloyWeight: Sequelize.FLOAT,//加工料重量
+	mixAlloyNumber: Sequelize.STRING,// 回炉锭炉号
+	mixAlloyWeight: Sequelize.FLOAT,// 回炉锭重量
+	highNbNumber: Sequelize.STRING,// 高铌料炉号
+	highNbWeight: Sequelize.FLOAT,// 高铌料重量
+	Si: Sequelize.FLOAT,// 硅重量
+	Ni: Sequelize.FLOAT,// Ni重量
+	Cu: Sequelize.FLOAT,// Cu重量
+	BFe: Sequelize.FLOAT,// 硼铁硅重量
+	NbFe: Sequelize.FLOAT,// 铌铁重量
+	alloyTotalWeight: Sequelize.FLOAT,//总重量
+	alloyOutWeight: Sequelize.FLOAT,// 放钢重量
+	alloyFixWeight: Sequelize.FLOAT,// 总重量修正
+	updatePerson: Sequelize.STRING, // 更新者
+	createPerson: Sequelize.STRING, // 创建者
+	remark: Sequelize.STRING, // 备注
+	createTime: Sequelize.DATE // 冶炼时间，00:00:00 - 08:00:00 之间的算上一天，不能算当天
 });
-
-meltSchema.index({furnace: 1});
-
-const Melt = mongoose.model('Melt', meltSchema);
 
 module.exports = Melt;
