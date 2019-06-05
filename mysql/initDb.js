cf0dc9229639d917f3ba084d0cf6517dfa546379
zhanghaiquan3 <zhanghaiquan3@guazi.com>
2019-06-05 21:02:53 +0800

    update

---
+++ mysql/initDb.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+const sequelize = require('./db');
+
+// Create Database struct from models
+sequelize
+    .sync({ alter: true })
+    .then(async () => {
+        console.log(
+            chalk.green('初始化数据库成功')
+        );
+    })
+    .catch(error => {
+        console.log(
+            chalk.red('初始化数据库失败: ', error)
+        );
+    });
+
