298f342957ca979a4d1f297965a38b4a380915d0
zhanghaiquan <zhqustb@163.com>
2020-10-15 00:47:05 +0800

    upd

--- controller/ribbonToughnessLevel.js
+++ controller/ribbonToughnessLevel.js
@@ -1,52 +1,52 @@
-'use strict';
+"use strict";
 
-const ribbonToughnessLevelModel = require('../models/ribbonToughnessLevel');
-const log = require('log4js').getLogger("ribbonToughnessLevel");
+const ribbonToughnessLevelModel = require("../models/ribbonToughnessLevel");
+const log = require("log4js").getLogger("ribbonToughnessLevel");
 
 class RibbonToughnessLevel {
-  constructor() {
-
-  }
+  constructor() {}
 
   async queryData(req, res, next) {
     try {
       // const list = await ribbonToughnessLevelModel.find({}).sort({'ribbonToughnessLevel': 'asc'});
       const list = await ribbonToughnessLevelModel.findAll({
-        order: [
-          ['ribbonToughnessLevel', 'asc']
-        ]
+        order: [["ribbonToughnessLevel", "asc"]],
       });
-      
+
       res.send({
         status: 0,
-        message: '操作成功',
+        message: "操作成功",
         data: {
-          list
-        }
+          list,
+        },
       });
     } catch (err) {
-      console.log('查询带材韧性失败', err);
-      log.error('查询带材韧性失败', err);
+      console.log("查询带材韧性失败", err);
+      log.error("查询带材韧性失败", err);
       res.send({
         status: -1,
-        message: '查询带材韧性失败'
+        message: "查询带材韧性失败",
       });
     }
   }
 
   async createData(req, res, next) {
-    const { ribbonToughness, ribbonToughnessLevel } = req.body;
-    try{
+    const {
+      ribbonToughness,
+      ribbonToughnessLevel,
+      ribbonToughnessLevelCode,
+    } = req.body;
+    try {
       if (!ribbonToughnessLevel || !ribbonToughness) {
-        throw new Error('参数错误')
+        throw new Error("参数错误");
       }
-    }catch(err){
+    } catch (err) {
       console.log(err.message, err);
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
-      })
+        message: err.message,
+      });
       return;
     }
 
@@ -54,79 +54,96 @@ class RibbonToughnessLevel {
       // const data = await ribbonToughnessLevelModel.findOne({ $or: [{ ribbonToughness }, { ribbonToughnessLevel }] });
       const data = await ribbonToughnessLevelModel.findOne({
         where: {
-          $or: [{ ribbonToughness }, { ribbonToughnessLevel }] 
-        }
+          $or: [
+            { ribbonToughness },
+            { ribbonToughnessLevel },
+            { ribbonToughnessLevelCode },
+          ],
+        },
       });
       // 如果没有查到则返回值为 null， 如果查询到则返回值为一个对象
       if (data) {
-        throw new Error('韧性描述或等级重复');
+        throw new Error("韧性描述或等级或PLC中code码重复");
       }
     } catch (err) {
       console.log(err.message, err);
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
-      })
+        message: err.message,
+      });
       return;
     }
 
     try {
       const newData = {
-        ribbonToughnessLevel, ribbonToughness
+        ribbonToughnessLevel,
+        ribbonToughness,
+        ribbonToughnessLevelCode,
       };
       await ribbonToughnessLevelModel.create(newData);
       res.send({
         status: 0,
-        message: '新增带材韧性成功'
+        message: "新增带材韧性成功",
       });
     } catch (err) {
-      console.log('新增带材韧性失败', err);
-      log.error('新增带材韧性失败', err);
+      console.log("新增带材韧性失败", err);
+      log.error("新增带材韧性失败", err);
       res.send({
         status: -1,
-        message: `新增带材韧性失败, ${err.message}`
+        message: `新增带材韧性失败, ${err.message}`,
       });
     }
   }
-  
+
   async updateData(req, res, next) {
-    const { ribbonToughnessLevelId, ribbonToughnessLevel, ribbonToughness } = req.body;
-    try{
-      if (!ribbonToughnessLevelId || !ribbonToughnessLevel || !ribbonToughness) {
-        throw new Error('参数错误')
+    const {
+      ribbonToughnessLevelId,
+      ribbonToughnessLevel,
+      ribbonToughness,
+      ribbonToughnessLevelCode,
+    } = req.body;
+    try {
+      if (
+        !ribbonToughnessLevelId ||
+        !ribbonToughnessLevel ||
+        !ribbonToughness ||
+        !ribbonToughnessLevelCode
+      ) {
+        throw new Error("参数错误");
       }
-    }catch(err){
+    } catch (err) {
       console.log(err.message, err);
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
-      })
+        message: err.message,
+      });
       return;
     }
     try {
       const newData = {
-        ribbonToughnessLevel, ribbonToughness
+        ribbonToughnessLevel,
+        ribbonToughness,
+        ribbonToughnessLevelCode,
       };
-      const [ n ] = await ribbonToughnessLevelModel.update(newData,
-      { 
-        where: { ribbonToughnessLevelId } 
+      const [n] = await ribbonToughnessLevelModel.update(newData, {
+        where: { ribbonToughnessLevelId },
       });
       if (n !== 0) {
         res.send({
           status: 0,
-          message: '更新带材韧性成功'
+          message: "更新带材韧性成功",
         });
       } else {
-        throw new Error('更新带材韧性失败')
+        throw new Error("更新带材韧性失败");
       }
     } catch (err) {
       console.log(err.message, err);
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
+        message: err.message,
       });
     }
   }
@@ -134,38 +151,38 @@ class RibbonToughnessLevel {
   // 删除
   async delData(req, res, next) {
     const { ribbonToughnessLevelId } = req.body;
-    try{
+    try {
       if (!ribbonToughnessLevelId) {
-        throw new Error('参数错误')
+        throw new Error("参数错误");
       }
-    }catch(err){
+    } catch (err) {
       console.log(err.message, err);
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
-      })
+        message: err.message,
+      });
       return;
     }
     try {
-      const n = await ribbonToughnessLevelModel.destroy({ 
+      const n = await ribbonToughnessLevelModel.destroy({
         where: {
-          ribbonToughnessLevelId
-        }
+          ribbonToughnessLevelId,
+        },
       });
       if (n !== 0) {
         res.send({
           status: 0,
-          message: '删除带材韧性成功'
+          message: "删除带材韧性成功",
         });
       } else {
-        throw new Error('删除带材韧性失败');
+        throw new Error("删除带材韧性失败");
       }
     } catch (err) {
       log.error(err.message, err);
       res.send({
         status: -1,
-        message: err.message
+        message: err.message,
       });
     }
   }
--- models/ribbonToughnessLevel.js
+++ models/ribbonToughnessLevel.js
@@ -1,29 +1,40 @@
-'use strict';
+"use strict";
 
-const Sequelize =  require('sequelize');
-const sequelize = require('../mysql/db');
-const moment = require('moment');
+const Sequelize = require("sequelize");
+const sequelize = require("../mysql/db");
+const moment = require("moment");
 
-const RibbonToughnessLevel = sequelize.define('ribbonToughnessLevel', {
-  ribbonToughnessLevelId: { 
+const RibbonToughnessLevel = sequelize.define("ribbonToughnessLevel", {
+  ribbonToughnessLevelId: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    primaryKey: true
+    primaryKey: true,
   },
   ribbonToughnessLevel: Sequelize.STRING,
   ribbonToughness: Sequelize.STRING,
+  ribbonToughnessLevelCode: Sequelize.INTEGER, // PLC中映射值
   createdAt: {
-		type: Sequelize.DATE,
-		get() {
-			return moment(this.getDataValue('createdAt')).format('YYYY-MM-DD HH:mm:ss');
-		}
-	},
-	updatedAt: {
-		type: Sequelize.DATE,
-		get() {
-			return moment(this.getDataValue('updatedAt')).format('YYYY-MM-DD HH:mm:ss');
-		}
-	}
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue("createdAt")).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
+    },
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue("updatedAt")).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
+    },
+  },
 });
+
+RibbonToughnessLevel.sync({ alter: true })
+  .then((result) => {})
+  .catch((err) => {
+    console.log("ribbonToughnessLevel表初始化失败", err.message);
+  });
 
 module.exports = RibbonToughnessLevel;
